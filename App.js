d37d7982f6178e71e96fa1b2a1414d281cb68d6f
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-09-14 22:37:22 -0300

    ex

--- App.js
+++ App.js
@@ -7,42 +7,27 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Text, View, Dimensions, Image } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const width = Dimensions.get('screen').width;
 
-export default class App extends Component {
+export default class Feed extends Component {
   render() {
+    const fotos = [
+      { id: 1, usuario: 'rafael' },
+      { id: 2, usuario: 'alberto' },
+      { id: 3, usuario: 'vitor' }
+    ];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Bem-vindo ao curso de React Native da Caelum!
-        </Text>
+      <View>
+        {fotos.map(foto =>
+          <View key={foto.id}>
+            <Text>{foto.usuario}</Text>
+            <Image source={require('./alura.png')}
+              style={{ width: width, height: width }} />
+          </View>
+        )}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
