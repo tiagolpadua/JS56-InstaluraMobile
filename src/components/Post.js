ffd1fa98c9ed8aa7e0d39ac2ef363be4eaa757c5
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-09-16 08:21:08 -0300

    ex

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../resources/img/s2-checked.png') :
+            require('../../resources/img/s2.png')
+    }
+
+    like = () => {
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        }
+        this.setState({ foto: fotoAtualizada });
+    }
+
     render() {
+        const { foto } = this.state;
         return (
             <View>
                 <View style={styles.cabecalho}>
-                    <Image source={{ uri: this.props.foto.urlPerfil }}
+                    <Image source={{ uri: foto.urlPerfil }}
                         style={styles.fotoDePerfil} />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
-                <Image source={{ uri: this.props.foto.urlFoto }}
+                <Image source={{ uri: foto.urlFoto }}
                     style={styles.foto} />
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={this.like}>
+                        <Image style={styles.botaoDeLike} source={this.carregaIcone(foto.likeada)} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -34,5 +61,12 @@ const styles = StyleSheet.create({
     foto: {
         width: width,
         height: width,
+    },
+    rodape: {
+        margin: 10,
+    },
+    botaoDeLike: {
+        height: 40,
+        width: 40,
     },
 });
