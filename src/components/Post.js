214552eb7f7bbd9b7b7bb603ad70d233c3aa6f24
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-09-15 22:22:40 -0300

    ex

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,15 +4,23 @@ import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
     render() {
+        const { foto } = this.state;
         return (
             <View>
                 <View style={styles.cabecalho}>
-                    <Image source={{ uri: this.props.foto.urlPerfil }}
+                    <Image source={{ uri: foto.urlPerfil }}
                         style={styles.fotoDePerfil} />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
-                <Image source={{ uri: this.props.foto.urlFoto }}
+                <Image source={{ uri: foto.urlFoto }}
                     style={styles.foto} />
             </View>
         );
