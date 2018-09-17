1ea2b33224802c256451150ce15054834b705e7a
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-09-16 23:55:03 -0300

    ex

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -16,11 +16,20 @@ export default class Feed extends Component {
       .then(json => this.setState({ fotos: json }));
   }
 
+  buscaPorId(idFoto) {
+    return this.state.fotos
+      .find(foto => foto.id === idFoto);
+  }
+
+  atualizaFotos(fotoAtualizada) {
+    return this.state.fotos
+      .map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto);
+  }
+
   adicionaComentario = (idFoto, valorComentario, inputComentario) => {
     if (valorComentario === '')
       return;
-    const foto = this.state.fotos
-      .find(foto => foto.id === idFoto);
+    const foto = this.buscaPorId(idFoto);
     const novaLista = [...foto.comentarios, {
       id: valorComentario,
       login: 'meuUsuario',
@@ -30,15 +39,13 @@ export default class Feed extends Component {
       ...foto,
       comentarios: novaLista,
     }
-    const fotos = this.state.fotos
-      .map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto);
+    const fotos = atualizaFotos(fotoAtualizada);
     this.setState({ fotos });
     inputComentario.clear();
   }
 
   like = (idFoto) => {
-    const foto = this.state.fotos
-      .find(foto => foto.id === idFoto);
+    const foto = this.buscaPorId(idFoto);
     let novaLista = [];
     if (!foto.likeada) {
       novaLista = [
@@ -55,8 +62,7 @@ export default class Feed extends Component {
       likeada: !foto.likeada,
       likers: novaLista
     }
-    const fotos = this.state.fotos
-      .map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto);
+    const fotos = atualizaFotos(fotoAtualizada);
     this.setState({ fotos });
   }
 
