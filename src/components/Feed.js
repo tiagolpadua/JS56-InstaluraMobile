a6cbf7a3f3c7079f6ae84e1d6bb8b676d0aef6eb
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2018-09-20 06:57:05 -0300

    ex4.4-async

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,10 +10,20 @@ export default class Feed extends Component {
     }
   }
 
+  async apiFetch() {
+    let resposta = await fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael');
+    let json = await resposta.json();
+    this.setState({ fotos: json });
+  }
+
   componentDidMount() {
+    /*
     fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
       .then(resposta => resposta.json())
       .then(json => this.setState({ fotos: json }));
+    */
+
+    this.apiFetch();
   }
 
   render() {
