669fe767d2a28f357d03f077e8e6588ca9389493
Tiago Pádua <tiagolpadua@gmail.com>
2018-11-18 15:21:23 -0200

    ex8.2

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -16,8 +16,38 @@ export default class Feed extends Component {
       .then(json => this.setState({ fotos: json }));
   }
 
+  buscaPorId(idFoto) {
+    return this.state.fotos.find(foto => foto.id === idFoto);
+  }
+
+  atualizaFotos(fotoAtualizada) {
+    return this.state.fotos.map(foto =>
+      foto.id === fotoAtualizada.id ? fotoAtualizada : foto
+    );
+  }
+
+  adicionaComentario = (idFoto, valorComentario, inputComentario) => {
+    if (valorComentario === "") return;
+    const foto = this.buscaPorId(idFoto);
+    const novaLista = [
+      ...foto.comentarios,
+      {
+        id: valorComentario,
+        login: "meuUsuario",
+        texto: valorComentario
+      }
+    ];
+    const fotoAtualizada = {
+      ...foto,
+      comentarios: novaLista
+    };
+    const fotos = this.atualizaFotos(fotoAtualizada);
+    this.setState({ fotos });
+    inputComentario.clear();
+  };
+
   like = idFoto => {
-    const foto = this.state.fotos.find(foto => foto.id === idFoto);
+    const foto = this.buscaPorId(idFoto);
     let novaLista = [];
     if (!foto.likeada) {
       novaLista = [...foto.likers, { login: "meuUsuario" }];
@@ -31,32 +61,8 @@ export default class Feed extends Component {
       likeada: !foto.likeada,
       likers: novaLista
     };
-    const fotos = this.state.fotos.map(foto =>
-      foto.id === fotoAtualizada.id ? fotoAtualizada : foto
-    );
+    const fotos = this.atualizaFotos(fotoAtualizada);
     this.setState({ fotos });
-  };
-
-  adicionaComentario = (idFoto, valorComentario, inputComentario) => {
-    if (valorComentario === "") return;
-    const foto = this.state.fotos.find(foto => foto.id === idFoto);
-    const novaLista = [
-      ...foto.comentarios,
-      {
-        id: valorComentario,
-        login: "meuUsuario",
-        texto: valorComentario
-      }
-    ];
-    const fotoAtualizada = {
-      ...foto,
-      comentarios: novaLista
-    };
-    const fotos = this.state.fotos.map(foto =>
-      foto.id === fotoAtualizada.id ? fotoAtualizada : foto
-    );
-    this.setState({ fotos });
-    inputComentario.clear();
   };
 
   render() {
